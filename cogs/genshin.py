02e4d9d3c3b2a509f5f2dd2a6011c09a67a14e33
Baby Aisha Maritza Virginia <78396352+LuminetteBourgeons@users.noreply.github.com>
2021-06-25 11:54:31 +0700

    2.6
    removing `characters` commands prototype...
    adding one more command...

--- cogs/genshin.py
+++ cogs/genshin.py
@@ -10,79 +10,11 @@ with open('json/characters.json') as json_file:
   f_characters = json.load(json_file)
 
 class Genshin(commands.Cog):   
-  def embed_character(self,character):
-    stars = ''
-    for i in range(character['rarity']):
-      stars += ' ⭐'
-    embed = discord.Embed(title=character['name'], description=stars, color=discord.Colour.orange())
-    embed.set_author(name='Character details')
-    embed.set_image(url=character['img'])
-    embed.add_field(name='Element', value=character['element'], inline=True)
-    embed.add_field(name='Weapon', value=character['weapon'], inline=True)
-    embed.add_field(name='Constellation', value=character['constellation'], inline=False)
-    return embed
-  def embed_character2(self,character):
-    stars = ''
-    for i in range(character['rarity']):
-      stars += ' ⭐'
-    embed = discord.Embed(title=character['name2'], description=stars, color=discord.Colour.orange())
-    embed.add_field(name='Constellation', value=character['cons2'], inline=False)
-    return embed
-  @commands.command(name='chara')
-  async def cmd_character(self, ctx, *keyword):
-    keyword = ' '.join(keyword).lower()
-    count = 0
-    find_list = []
-    find_one = None
-    emoji = '⬇️'
-    for character in f_characters:
-      name = character['name'].lower()
-      if keyword.lower() == character['name'].lower():
-        embed = self.embed_character(character)
-        embed.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
-        message = await ctx.send(embed=embed)
-        await message.add_reaction(emoji)
-        while True:
-          reaction = await ctx.bot.wait_for_reaction(emoji="⬇️", message=message, timeout=30)
-          embed = self.embed_character2(character)
-          embed.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
-          await message.edit(content=embed)
-        return
-      elif len(keyword) >= 4:
-        if name.find(keyword) >= 0:
-          find_one = character
-          find_list.append(character)
-          count += 1
-    if count == 0:
-      embed = discord.Embed(description="Character not found",colour=discord.Colour.red())
-      embed.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
-      await ctx.send(embed=embed)
-    elif count == 1:
-      embed = self.embed_character(find_one)
-      embed.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
-      message = await ctx.send(embed=embed)
-      await message.add_reaction(emoji)
-      while True:
-        reaction = await ctx.bot.wait_for_reaction(emoji="⬇️", message=message)
-        embed = self.embed_character2(find_one)
-        await message.edit(content=embed)
-    else:
-      character_list = ''
-      for character in find_list:
-        character_list += f"{character['name']}\n"
-      embed = discord.Embed(color=discord.Colour.orange())
-      embed.set_author(name=f"{count} characters found\nPlease choose one")
-      embed.add_field(name="Syntax : `+ chara {name}`", value=character_list, inline=False)
-      embed.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
-      await ctx.send(embed=embed)
-    if count != 0:
-      return
-        
   def embed_weapon(self, weapon):
     stars = ''
     for i in range(weapon['rarity']):
       stars += ' ⭐'
-    embed = discord.Embed(title=weapon['name'], description=stars, color=discord.Colour.orange())
+    embed = discord.Embed(title=weapon['name'], description=stars, color=discord.Color.orange())
     embed.set_author(name='Weapon details')
     embed.set_thumbnail(url=weapon['img'])
     embed.add_field(name='Type', value=weapon['type'], inline=True)
@@ -93,8 +25,8 @@ class Genshin(commands.Cog):
     embed.add_field(name='Location', value=weapon['location'], inline=True)
     embed.add_field(name='Bonus', value=weapon['bonus'], inline=False)
     return embed
-  @commands.command(name='w')
-  async def cmd_weapon(self, ctx, *keyword):
+  @commands.command(aliases=['w'])
+  async def weapon(self, ctx, *keyword):
     keyword = ' '.join(keyword).lower()
     count = 0
     find_list = []
@@ -123,7 +55,7 @@ class Genshin(commands.Cog):
       weapon_list = ''
       for weapon in find_list:
         weapon_list += f"{weapon['name']}\n"
-      embed = discord.Embed(color=discord.Colour.orange())
+      embed = discord.Embed(color=discord.Color.orange())
       embed.set_author(name=f"{count} weapons found\nPlease choose one")
       embed.add_field(name="Syntax : `+ weapon {name}`", value=weapon_list, inline=False)
       embed.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
@@ -133,15 +65,15 @@ class Genshin(commands.Cog):
 
   def embed_artifact(self, artifact):
     stars = ''
-    embed = discord.Embed(title=artifact['set'], description=stars, color=discord.Colour.orange())
+    embed = discord.Embed(title=artifact['set'], description=stars, color=discord.Color.orange())
     embed.set_author(name='Artifact details')
     embed.set_thumbnail(url=artifact['img'])
     embed.add_field(name='Max Rarity', value=artifact['max-rarity'], inline=False)
     embed.add_field(name='2-Piece Bonus', value=artifact['2-set bonus'], inline=False)
     embed.add_field(name='4-Piece Bonus', value=artifact['4-set bonus'], inline=False)
     return embed
-  @commands.command(name='a')
-  async def cmd_artifact(self, ctx, *keyword):
+  @commands.command(aliases=['a'])
+  async def artifact(self, ctx, *keyword):
     keyword = ' '.join(keyword).lower()
     count = 0
     find_list = []
@@ -170,7 +102,7 @@ class Genshin(commands.Cog):
       artifact_list = ''
       for artifact in find_list:
         artifact_list += f"{artifact['set']}\n"
-      embed = discord.Embed(color=discord.Colour.orange())
+      embed = discord.Embed(color=discord.Color.orange())
       embed.set_author(name=f"{count} artifact found\nPlease choose one")
       embed.add_field(name="Syntax : `+ artifact {name}`", value=artifact_list, inline=False)
       embed.set_footer(text=f"Serving: {ctx.author.name}", icon_url=ctx.author.avatar_url)
@@ -178,9 +110,13 @@ class Genshin(commands.Cog):
     if count != 0:
       return
 
-    @commands.command()
-    async def exp(self, ctx):
-      await ctx.send("https://cdn.discordapp.com/attachments/637264326368952323/807527453303898152/exp.png")
+  @commands.command()
+  async def exp(self, ctx):
+    await ctx.send("https://cdn.discordapp.com/attachments/637264326368952323/807527453303898152/exp.png")
+  
+  @commands.command(aliases=['talent'])
+  async def weekly(self, ctx):
+    await ctx.send("https://media.discordapp.net/attachments/840489159642841118/855088556836782120/Talent_eng.png")
       
 def setup(bot):
     bot.add_cog(Genshin(bot))
